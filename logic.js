3da081c546b191e12abaee5817ccc7e6ebb2f005
Jordans Laptop <jordank.worley@gmail.com>
2024-06-22 02:40:12 -0400

    Added random number gen to String for computer

--- logic.js
+++ logic.js
@@ -16,6 +16,8 @@ function playRound(humanChoice, computerChoice) {
 
     computerChoice = getComputerChoice()
 
+    let Answer = null
+
     // This section converts the computer answer(float) to rock, paper, or scissor.
 
     if (computerChoice >= .6666) {
@@ -32,19 +34,21 @@ function playRound(humanChoice, computerChoice) {
 
     // This section prevents any grammatical errors from popping up.
 
-    if (humanChoice = 'Rock' || 'rock' || 'ROCK' ) {
-         Answer = Rock
+    if (humanChoice == 'Rock' || 'rock' || 'ROCK' ) {
+        Answer = 'Rock'
     }
 
-    else if (humanChoice = 'Paper' || 'paper' || 'PAPER' ) {
-        Answer = Paper
+    else if (humanChoice == 'Paper' || 'paper' || 'PAPER' ) {
+        Answer = 'Paper'
     }
 
-    else if (humanChoice = Scissors || scissors || SCISSORS) {
-        Answer = Scissors
+    else if (humanChoice == 'Scissors' || 'scissors' || 'SCISSORS') {
+        Answer = 'Scissors'
     }
 
-    else (Alert('That is not a valid answer!'))
+    else(alert('That is not a valid answer!'))
+
+    console.log(Answer)
 }
 
 playRound()
