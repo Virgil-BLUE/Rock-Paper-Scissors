b3828e687b95e9acf5240a626fd8caac3d3b88f3
Jordans Laptop <jordank.worley@gmail.com>
2024-07-22 01:56:27 -0400

    Completed game. Added comments.

--- logic.js
+++ logic.js
@@ -1,22 +1,88 @@
 // Global Variables //
 
+let ComputerChoice = undefined;
 
+let HumanChoice = undefined;
 
+let humanScore = 0;
+
+let computerScore = 0;
+
+let gameRounds = 0;
+
+// Determines Computer Move 
 
 function getComputerChoice() {
 
-    let random_num = Math.random()
-    let ComputerChoice = undefined
-    
-    if (random_num < .33) {
-        let ComputerChoice = 'rock'
-    } else if (random_num >= .33 && random_num < .66) {
-        let ComputerChoice = 'paper'
-    } else  {
-        let ComputerChoice = 'scissors'
-    }
+    let random_num = Math.random();
 
-    console.log(random_num)
+    if (random_num < .33) {
+        ComputerChoice = 'rock';
+    } else if (random_num >= .33 && random_num < .66) {
+        ComputerChoice = 'paper';
+    } else  {
+        ComputerChoice = 'scissors';
+    }
 }
 
-getComputerChoice()
+// Prompts player for choice, corrects grammar to limit parameters.
+
+function getHumanChoice() {
+    
+    HumanChoice = prompt('rock, paper, or scissors?');
+
+    if (HumanChoice === 'rock' || HumanChoice === 'Rock' || HumanChoice === 'ROCK' ) {
+        HumanChoice = 'rock'
+    } else if (HumanChoice === 'paper' || HumanChoice === 'Paper' || HumanChoice === 'PAPER') {
+        HumanChoice = 'paper'
+    } else if (HumanChoice === 'scissors' || HumanChoice === 'Scissors' || HumanChoice === 'SCISSORS') {
+        HumanChoice = 'scissors'
+    } else {alert('Sorry, thats not a valid answer. Try again!')}
+
+}
+
+// Game logic and Score Adder
+
+function playRound(HumanChoice, ComputerChoice) {
+    
+    if (HumanChoice === ComputerChoice) {
+        alert('Draw!')
+    } else if ((HumanChoice === 'rock' && ComputerChoice === 'scissors') || (HumanChoice === 'paper' && ComputerChoice === 'rock') || (HumanChoice === 'scissors' && ComputerChoice === 'paper')) {
+        alert('Congratulations, you won that round. The computers choice was ' + ComputerChoice)
+        humanScore++; 
+    } else {
+        alert('Sorry, You lost that round. The computers choice was ' + ComputerChoice)
+        computerScore++;
+    }
+    
+}
+
+// Game Initialization and Score Logic 
+
+function playGame() {
+
+    getComputerChoice()
+    getHumanChoice()
+    playRound(HumanChoice, ComputerChoice)
+    
+}
+
+// Game Looping Mechanic 
+
+for (gameRounds; gameRounds < 5; gameRounds++) {
+    
+    playGame()
+    console.log(humanScore)
+    console.log(computerScore)
+    
+} 
+
+// Game Results Decleration
+
+if (computerScore > humanScore) {
+    alert('Sorry, you lost!')
+} else if (computerScore == humanScore) {
+    alert('Its a draw!')
+} else if (computerScore < humanScore) {
+    alert ('Congratulations, you won!')
+}
